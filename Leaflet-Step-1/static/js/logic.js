ece7ccc6908bcc6a769b0b39a85b84fa9869a86c
Alysia Dugan <81889411+alysiadugan4@users.noreply.github.com>
2021-11-10 18:11:12 -0700

    Update JS

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -26,3 +26,32 @@ function changeColor(feature) {
       return "lightgreen";
     }
   }
+
+  function createFeatures(earthquakeData) {
+  
+    
+    function onEachFeature(feature, layer) {
+  
+      layer.bindPopup(`<h3>${feature.properties.place}</h3><hr>Magnitude: ${feature.properties.mag}<br> Depth: ${feature.geometry.coordinates[2]}`)
+    }
+  
+    let earthquakes = L.geoJson(earthquakeData, {
+  
+      pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng);
+      },
+      style: function(feature) {
+        return {
+          color: "white",
+          weight: 0.3,
+          radius: feature.properties.mag * 5,
+          fillOpacity: .5,
+          fillColor: changeColor(feature)
+        }
+      },
+      onEachFeature: onEachFeature
+    })
+  
+    // Pass the earthquake data to a createMap() function.
+    createMap(earthquakes);
+  }
